367b3c8d8febb9fc3a682f527928f7ba4d18d3dc
John Fadich <jofad4@gmail.com>
2015-07-03 22:18:18 -0700

    rds

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+            'host'     => env( 'RDS_HOSTNAME', 'localhost' ),
+            'database' => env( 'RDS_DB_NAME', 'forge' ),
+            'username' => env( 'RDS_USERNAME', 'forge' ),
+            'password' => env( 'RDS_PASSWORD', '' ),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

11e49735cf6a6cf1b1298705920751fef737fa53
John Fadich <jofad4@gmail.com>
2016-01-02 20:07:31 -0800

    composer update

--- app/DBLogger/DBLogger.php
+++ app/DBLogger/DBLogger.php
@@ -63,4 +63,39 @@ class DBLogger
         return json_encode( $_GET );
     }
 
+    private function getApplicationHash()
+    {
+        return $this->hashDirectory( app_path() ) . '.' . $this->hashDirectory( database_path( 'migrations' ) );
+    }
+
+    /**
+     * Generate an MD5 hash string from the contents of a directory.
+     *
+     * @param string $directory
+     * @return boolean|string
+     */
+    private function hashDirectory( $directory )
+    {
+        if ( !is_dir( $directory ) ) {
+            return false;
+        }
+
+        $files = array();
+        $dir   = dir( $directory );
+
+        while ( false !== ( $file = $dir->read() ) ) {
+            if ( $file != '.' and $file != '..' ) {
+                if ( is_dir( $directory . '/' . $file ) ) {
+                    $files[] = hashDirectory( $directory . '/' . $file );
+                } else {
+                    $files[] = md5_file( $directory . '/' . $file );
+                }
+            }
+        }
+
+        $dir->close();
+
+        return md5( implode( '', $files ) );
+    }
+
 }
